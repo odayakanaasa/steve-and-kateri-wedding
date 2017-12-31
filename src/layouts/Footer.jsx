753cb83296d6b87fb9ae48145ed8f70e50c81548
Steve Frost <sfrost@nerdery.com>
2017-12-30 22:58:48 -0600

    added Footer component

---
+++ src/layouts/Footer.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import "../css/Footer.css";
+
+class Footer extends Component {
+    render() {
+        return (
+            <footer>
+                <p>Get Excited • Created w/ &#9829; by Steve</p>
+            </footer>
+        );
+    }
+}
+
+export default Footer;
