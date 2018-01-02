f6143be7830c5a7a905ca1254f0a76489d57f475
Steve Frost <sfrost@nerdery.com>
2018-01-01 22:42:07 -0600

    implement Netlify CMS

--- src/html.jsx
+++ src/html.jsx
@@ -37,6 +37,7 @@ export default class HTML extends React.Component {
           {this.props.headComponents}
           <link rel="shortcut icon" href={favicon} />
           {css}
+          <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
         </head>
         <body>
           <div
