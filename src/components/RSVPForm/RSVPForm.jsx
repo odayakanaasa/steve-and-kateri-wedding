f6b877da8f9446da2d854b8f50761ce5773d3459
Steve Frost <sfrost@nerdery.com>
2018-03-17 17:51:42 -0500

    last typo, I'm sure

--- src/components/RSVPForm/RSVPForm.jsx
+++ src/components/RSVPForm/RSVPForm.jsx
@@ -66,7 +66,6 @@ const RSVPForm = () => (
             <option value="0" selected>0</option>
             <option value="1">1</option>
             <option value="2">2</option>
-            <option value="2">2</option>
             <option value="3">3</option>
             <option value="4">4</option>
             <option value="5">5</option>
