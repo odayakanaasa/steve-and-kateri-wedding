d82325f6a175eb449c0794c7cdeb2cbb8226f1ba
Steve Frost <sfrost@nerdery.com>
2018-03-27 11:08:10 -0500

    fixed redirect url

--- src/components/RSVPForm/RSVPForm.jsx
+++ src/components/RSVPForm/RSVPForm.jsx
@@ -4,7 +4,7 @@ import './RSVPForm.css';
 const RSVPForm = () => (
   <div>
     <form
-      action="/rsvp-confirmation"
+      action="/rsvp-confirmation/"
       method="POST"
       className="rsvp-form"
       data-netlify="true"
